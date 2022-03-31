1b0d32edec56a6028a616bc85978d160a739a2c6
John Rogers <john.rogers@osufoundation.org>
2022-03-31 11:28:35 -0700

    Add Airflow extras

--- build_image.sh
+++ build_image.sh
@@ -3,8 +3,7 @@ CMD=(
     docker build .
     --build-arg PYTHON_BASE_IMAGE="python:3.9-slim-buster"
     --build-arg AIRFLOW_VERSION="2.2.4"
-    # --build-arg AIRFLOW_EXTRAS="amazon,async,celery,cncf.kubernetes,dask,ftp,grpc,http,ldap,microsoft.azure,odbc,postgres,redis,sftp,ssh,statsd,virtualenv,opsgenie,snowflake,microsoft.mssql"
-    --build-arg AIRFLOW_EXTRAS="async,celery,dask,grpc,postgres,redis,statsd,microsoft.azure,microsoft.mssql,sftp,opsgenie,cncf.kubernetes"
+    --build-arg AIRFLOW_EXTRAS="async,grpc,postgres,statsd,microsoft.azure,microsoft.mssql,sftp,opsgenie,cncf.kubernetes,snowflake,celery,amazon,redis,ldap"
     --build-arg INSTALL_MYSQL_CLIENT="false"
     --build-arg ADDITIONAL_RUNTIME_APT_DEPS="sshpass"
     --tag "osufcontainerregistry.azurecr.io/airflow:latest"
