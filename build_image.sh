57370d4046b59599e800efcc2f5bf6169aa6d185
John Rogers <john.rogers@osufoundation.org>
2022-04-04 16:16:24 -0700

    Bump to Airflow 2.2.5

--- build_image.sh
+++ build_image.sh
@@ -2,7 +2,7 @@
 CMD1=(
     docker build .
     --build-arg PYTHON_BASE_IMAGE="python:3.9-slim-buster"
-    --build-arg AIRFLOW_VERSION="2.2.4"
+    --build-arg AIRFLOW_VERSION="2.2.5"
     --build-arg AIRFLOW_EXTRAS="async,grpc,postgres,statsd,microsoft.azure,microsoft.mssql,sftp,opsgenie,cncf.kubernetes,snowflake,celery,amazon,redis,ldap"
     --build-arg INSTALL_MYSQL_CLIENT="false"
     --build-arg ADDITIONAL_RUNTIME_APT_DEPS="sshpass"
