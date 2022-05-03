c87bff9661a6c8333c970fc8c931a87951c0659f
John Rogers <john.rogers@osufoundation.org>
2022-05-03 09:52:37 -0700

    Merge changes from 2.2 branch

---
+++ build_image.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+CMD1=(
+    docker build .
+    --build-arg PYTHON_BASE_IMAGE="python:3.9-slim-buster"
+    --build-arg AIRFLOW_VERSION="2.2.5"
+    --build-arg AIRFLOW_EXTRAS="async,grpc,postgres,statsd,microsoft.azure,microsoft.mssql,sftp,opsgenie,cncf.kubernetes,snowflake,celery,amazon,redis,ldap"
+    --build-arg INSTALL_MYSQL_CLIENT="false"
+    --build-arg ADDITIONAL_RUNTIME_APT_DEPS="sshpass"
+    # --tag "osufcontainerregistry.azurecr.io/airflow:latest"
+    --tag "airflow-intermediate:latest"
+)
+CMD2=(
+    docker build --file Dockerfile-osuf .
+    --tag "osufcontainerregistry.azurecr.io/airflow:latest"
+)
+
+"${CMD1[@]}" && "${CMD2[@]}" 
