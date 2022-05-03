36df4b505cad866c691460bdfd320ac512f4a1cf
John Rogers <john.rogers@osufoundation.org>
2022-05-03 09:58:29 -0700

    Update permissions

--- build_image_test.sh
+++ build_image_test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 CMD1=(
     docker build .
-    --build-arg PYTHON_BASE_IMAGE="python:3.9-slim-buster"
-    --build-arg AIRFLOW_VERSION="2.2.5"
+    --build-arg PYTHON_BASE_IMAGE="python:3.10-slim-buster"
+    --build-arg AIRFLOW_VERSION="2.3.0"
     --build-arg AIRFLOW_EXTRAS="async,grpc,postgres,statsd,microsoft.azure,microsoft.mssql,sftp,opsgenie,cncf.kubernetes,snowflake,celery,amazon,redis,ldap"
     --build-arg INSTALL_MYSQL_CLIENT="false"
     --build-arg ADDITIONAL_RUNTIME_APT_DEPS="sshpass"
