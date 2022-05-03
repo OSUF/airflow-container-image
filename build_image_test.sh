b290f9ff514be7b24289b299c5de2733aa7bed00
John Rogers <john.rogers@osufoundation.org>
2022-05-03 11:29:11 -0700

    Update dockerfile build from

--- build_image_test.sh
+++ build_image_test.sh
@@ -6,11 +6,11 @@ CMD1=(
     --build-arg AIRFLOW_EXTRAS="async,grpc,postgres,statsd,microsoft.azure,microsoft.mssql,sftp,opsgenie,cncf.kubernetes,snowflake,celery,amazon,redis,ldap"
     --build-arg INSTALL_MYSQL_CLIENT="false"
     --build-arg ADDITIONAL_RUNTIME_APT_DEPS="sshpass"
-    # --tag "osufcontainerregistry.azurecr.io/airflow:latest"
     --tag "airflow-intermediate:test"
 )
 CMD2=(
     docker build --file Dockerfile-osuf .
+    --build-arg ENV="test"
     --tag "osufcontainerregistry.azurecr.io/airflow:test"
 )
 
