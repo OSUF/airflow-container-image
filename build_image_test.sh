435e7853a4c247ce499b8def27984101020b0caf
John Rogers <john.rogers@osufoundation.org>
2022-05-11 10:55:04 -0700

    Add custom files

---
+++ build_image_test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+CMD1=(
+    docker build .
+    --build-arg PYTHON_BASE_IMAGE="python:3.9-slim-buster"
+    --build-arg AIRFLOW_VERSION="2.3.0"
+    --build-arg AIRFLOW_EXTRAS="async,postgres,microsoft.azure,microsoft.mssql,sftp,opsgenie,snowflake,celery,amazon,redis,ldap"
+    --build-arg INSTALL_MYSQL_CLIENT="false"
+    --build-arg ADDITIONAL_RUNTIME_APT_DEPS="sshpass"
+    --tag "airflow-intermediate:test"
+    --progress plain
+)
+CMD2=(
+    docker build --file Dockerfile-osuf .
+    --build-arg ENV="test"
+    --tag "osufcontainerregistry.azurecr.io/airflow:test"
+    --progress plain
+)
+
+"${CMD1[@]}" && "${CMD2[@]}" 
