c2d8998259396d632775e01a2b83f6549cd24023
John Rogers <john.rogers@osufoundation.org>
2022-05-13 08:56:25 -0700

    Revert to 2.2.5

--- build_image_test.sh
+++ build_image_test.sh
@@ -2,7 +2,7 @@
 CMD1=(
     docker build .
     --build-arg PYTHON_BASE_IMAGE="python:3.9-slim-buster"
-    --build-arg AIRFLOW_VERSION="2.3.0"
+    --build-arg AIRFLOW_VERSION="2.2.5"
     --build-arg AIRFLOW_EXTRAS="async,postgres,microsoft.azure,microsoft.mssql,sftp,opsgenie,snowflake,celery,amazon,redis,ldap,odbc"
     --build-arg INSTALL_MYSQL_CLIENT="false"
     --build-arg ADDITIONAL_RUNTIME_APT_DEPS="sshpass"
