af9bea3523c26a394a1f051547e232ebd11080b7
John Rogers <john.rogers@osufoundation.org>
2022-05-16 13:16:48 -0700

    Change to local k8s

--- build_image.sh
+++ build_image.sh
@@ -3,15 +3,17 @@ CMD1=(
     docker build .
     --build-arg PYTHON_BASE_IMAGE="python:3.9-slim-buster"
     --build-arg AIRFLOW_VERSION="2.2.5"
-    --build-arg AIRFLOW_EXTRAS="async,grpc,postgres,statsd,microsoft.azure,microsoft.mssql,sftp,opsgenie,cncf.kubernetes,snowflake,celery,amazon,redis,ldap"
+    --build-arg AIRFLOW_EXTRAS="async,postgres,microsoft.azure,microsoft.mssql,sftp,opsgenie,snowflake,celery,amazon,redis,ldap,odbc"
     --build-arg INSTALL_MYSQL_CLIENT="false"
     --build-arg ADDITIONAL_RUNTIME_APT_DEPS="sshpass"
     --tag "airflow-intermediate:latest"
+    --progress plain
 )
 CMD2=(
     docker build --file Dockerfile-osuf .
     --build-arg ENV="latest"
-    --tag "osufcontainerregistry.azurecr.io/airflow:latest"
+    --tag "p1lv01k8s.osufoundation.org:32000/airflow:latest"
+    --progress plain
 )
 
 "${CMD1[@]}" && "${CMD2[@]}" 
