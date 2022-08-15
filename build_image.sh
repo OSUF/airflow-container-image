8e2503478cb39865a1af10ca5fb036cc7a1665f6
John Rogers <john.rogers@osufoundation.org>
2022-08-15 09:35:47 -0700

    Add version variable

--- build_image.sh
+++ build_image.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+
+AIRFLOW_VERSION="2.3.3"
+
 CMD1=(
     docker build .
     --build-arg PYTHON_BASE_IMAGE="python:3.9-slim-buster"
-    --build-arg AIRFLOW_VERSION="2.3.2"
+    --build-arg AIRFLOW_VERSION=$AIRFLOW_VERSION
     --build-arg AIRFLOW_EXTRAS="async,postgres,microsoft.azure,microsoft.mssql,sftp,opsgenie,snowflake,celery,amazon,redis,ldap,odbc"
     --build-arg INSTALL_MYSQL_CLIENT="false"
     --build-arg ADDITIONAL_RUNTIME_APT_DEPS="sshpass"
@@ -12,8 +15,11 @@ CMD1=(
 CMD2=(
     docker build --file Dockerfile-osuf .
     --build-arg ENV="prod"
-    --tag "p1lv01k8s.osufoundation.org:32000/airflow:2.3.2"
+    --tag "p1lv01k8s.osufoundation.org:32000/airflow:$AIRFLOW_VERSION"
     --progress plain
 )
+CMD3=(
+    docker push p1lv01k8s.osufoundation.org:32000/airflow:$AIRFLOW_VERSION
+)
 
-"${CMD1[@]}" && "${CMD2[@]}" 
+"${CMD1[@]}" && "${CMD2[@]}" && "${CMD3[@]}"
