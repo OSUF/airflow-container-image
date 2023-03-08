3521ba28ee29cd9628be2c7c39c2b3160357e17c
John Rogers <john.rogers@osufoundation.org>
2023-03-08 13:04:46 -0800

    Add image for local development

---
+++ build_image_local.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+AIRFLOW_VERSION="2.5.1"
+
+CMD1=(
+    docker build .
+    --build-arg PYTHON_BASE_IMAGE="python:3.9-slim-bullseye"
+    --build-arg AIRFLOW_VERSION=$AIRFLOW_VERSION
+    --build-arg AIRFLOW_EXTRAS="async,postgres,microsoft.azure,microsoft.mssql,sftp,opsgenie,snowflake,celery,amazon,redis,ldap,odbc,pandas"
+    --build-arg INSTALL_MYSQL_CLIENT="false"
+    --build-arg ADDITIONAL_RUNTIME_APT_DEPS="sshpass"
+    --tag "airflow-intermediate:test"
+    --progress plain
+)
+CMD2=(
+    docker build --file Dockerfile-osuf .
+    --build-arg ENV="test"
+    --tag "localhost:32000/airflow:$AIRFLOW_VERSION"
+    --progress plain
+)
+CMD3=(
+    docker push localhost:32000/airflow:$AIRFLOW_VERSION
+)
+
+"${CMD1[@]}" && "${CMD2[@]}" && "${CMD3[@]}"
